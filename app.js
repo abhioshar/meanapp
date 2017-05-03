e8623a18a6922cd01979512f9bf322e61b3ad57a
abhioshar <abhioshar@github.com>
2017-05-03 12:23:38 +0530

    Add user model and registration

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 const config = require('./config/database');
 
+// Connect to the mongodb database using configuration
 mongoose.connect(config.database);
 
+// Database connection is successful
 mongoose.connection.on('connected', () => {
 	console.log('Connected to database ' + config.database);
 });
 
+// Database connection is unsuccessful
 mongoose.connection.on('error', (err) => {
 	console.log('Database error: ' + err);
 });
@@ -22,18 +25,24 @@ const users = require('./routes/users');
 
 const port = 3000;
 
+// For allowing cross domain requests
 app.use(cors());
 
+// Static path
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Bodyparser for parsing incoming request bodies
 app.use(bodyParser.json());
 
+// Setting route for users
 app.use('/users', users);
 
+// Root path
 app.get('/', (req, res) => {
 	res.send('Invalid Endpoint');
 });
 
+// Start the app
 app.listen(port, () => {
 	console.log('Server started on port ' + port);
 });
--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
-router.get('/register', (req, res, next) => {
-	res.send('register');
+const User = require('../models/user');
+
+// Register
+router.post('/register', (req, res, next) => {
+	let newUser = new User({
+		name: req.body.name,
+		email: req.body.email,
+		username: req.body.username,
+		password: req.body.password
+	});
+
+	User.addUser(newUser, (err, user) => {
+		if(err) {
+			res.json({
+				success: false,
+				msg: 'Failed to register user'
+			});
+		} else {
+			res.json({
+				success: true,
+				msg: 'Successfully registered user'
+			});
+		}
+	});
 });
 
+// Authenticate
 router.post('/authenticate', (req, res, next) => {
 	res.send('authenticate');
 });
 
+// Profile
 router.get('/profile', (req, res, next) => {
 	res.send('profile');
-});
-
-router.get('/validate', (req, res, next) => {
-	res.send('validate');
 });
 
 module.exports = router;
