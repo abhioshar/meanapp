dbacaa05ca7afca822ebea1eb62ec786079a977f
abhioshar <abhioshar@github.com>
2017-05-11 17:22:29 +0530

    Configure http post url

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -12,14 +12,16 @@ export class AuthService {
   registerUser(user) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.post('http://localhost:8080/users/register', user, {headers: headers})
+    // return this.http.post('http://localhost:8080/users/register', user, {headers: headers})
+    return this.http.post('users/register', user, {headers: headers})
       .map(res => res.json());
   }
 
   authenticateUser(user) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.post('http://localhost:8080/users/authenticate', user, {headers: headers})
+    // return this.http.post('http://localhost:8080/users/authenticate', user, {headers: headers})
+    return this.http.post('users/authenticate', user, {headers: headers})
       .map(res => res.json());
   }
 
