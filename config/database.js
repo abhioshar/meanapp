5586042fe1579c2797467192b27ab5a421fa2aad
abhioshar <abhioshar@github.com>
2017-05-11 17:21:52 +0530

    Add mongodb on heroku

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-	database: 'mongodb://localhost:27017/meanauth',
+	//database: 'mongodb://localhost:27017/meangameapp',
+	database: 'mongodb://<dbuser>:<dbpassword>@ds137101.mlab.com:37101/meangameapp',
 	secret: 'yoursecret'
 }
