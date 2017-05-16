2b1b1716a6bb21b08429196d9478444642e67b83
abhioshar <abhioshar@github.com>
2017-05-16 15:07:19 +0530

    Add gameservice to dashboard component

--- angular-src/src/app/component/dashboard/dashboard.component.ts
+++ angular-src/src/app/component/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { GameService } from '../../services/game.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
+  providers: [GameService]
 })
 export class DashboardComponent implements OnInit {
 
