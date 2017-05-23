46a3dcc8f1d18f6ccc8f84458b19948cb9b3dc29
abhioshar <abhioshar@github.com>
2017-05-23 20:55:03 +0530

    Add end to end tests for all components

--- angular-src/e2e/app.e2e-spec.ts
+++ angular-src/e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AngularSrcPage } from './app.po';
+import { element, by, browser } from 'protractor';
 
 describe('angular-src App', function() {
   let page: AngularSrcPage;
@@ -7,8 +8,64 @@ describe('angular-src App', function() {
     page = new AngularSrcPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display message saying MEAN App', () => {
     page.navigateTo();
     expect(page.getParagraphText()).toEqual('MEAN App');
+  });
+});
+
+describe('login page', function() {
+  let page: AngularSrcPage;
+
+  beforeEach(() => {
+    page = new AngularSrcPage();
+  });
+
+  it('should display header saying Login', () => {
+    browser.get('/login');
+    let header = element(by.tagName('h2')).getText();
+    expect(header).toEqual('Login');
+  });
+});
+
+describe('register page', function() {
+  let page: AngularSrcPage;
+
+  beforeEach(() => {
+    page = new AngularSrcPage();
+  });
+
+  it('should display header saying Register', () => {
+    browser.get('/register');
+    let header = element(by.tagName('h2')).getText();
+    expect(header).toEqual('Register');
+  });
+});
+
+describe('dashboard page', function() {
+  let page: AngularSrcPage;
+
+  beforeEach(() => {
+    page = new AngularSrcPage();
+  });
+
+  it('should redirect dashboard to login saying Login', () => {
+    browser.get('/dashboard');
+    let header = element(by.tagName('h2')).getText();
+    expect(header).toEqual('Login');
+  });
+});
+
+describe('profile page', function() {
+  let page: AngularSrcPage;
+
+  beforeEach(() => {
+    page = new AngularSrcPage();
+  });
+
+  it('should redirect profile to login saying Login', () => {
+    browser.get('/profile');
+    let header = element(by.tagName('h2')).getText();
+    expect(header).toEqual('Login');
   });
 });
