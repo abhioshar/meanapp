82860396be23b8541898931b1038448a8369f292
abhioshar <abhioshar@github.com>
2017-05-23 16:13:44 +0530

    End to end testing

--- angular-src/e2e/app.e2e-spec.ts
+++ angular-src/e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('angular-src App', function() {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('MEAN App');
   });
 });
